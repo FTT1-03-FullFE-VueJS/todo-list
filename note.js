78d371e3696b11f30ced642606c1f3177cd4a8af
cba_howard <cba_howard@cybridge.jp>
2022-02-12 18:30:35 +0700

    Handle feature sort

--- note.js
+++ note.js
@@ -40,3 +40,167 @@
  * file: app.js
  * --> import fun1, { subFun1, subFun2 } from 'export.js'
  */
+
+// ===========================================================================
+/**
+ * Hoisting strong javascript
+ * -> Có thể được hỏi lúc phổng vấn
+ * var
+ * let
+ * const
+ */
+
+/**
+ * vd: 1
+ */
+// Đề
+// console.log(name); // undefined
+// console.log(age); // error: age is not defined
+// var name = 20;
+
+// Javascript
+// var name; // <=> var name = undefined
+// console.log(name);// undefined
+// console.log(age);
+// name = 20;
+
+// console.log(name); // 20, => error: Cannot access 'name' before initialization
+// console.log(age); // error: age is not defined
+// let name = 20;
+
+// console.log(name); // 20, => error: Cannot access 'name' before initialization
+// console.log(age); // error: age is not defined
+// name = 20;
+
+// console.log(getFullName()); // pham dinh hung
+
+// function getFullName() {
+//   return 'pham dinh hung';
+// }
+
+// //
+// function getFullName() {
+//   return 'pham dinh hung';
+// }
+
+// console.log(getFullName()); // pham dinh hung
+
+// ==============================================
+
+// let users = ['hung', 'nhan', 'thuy', 'ngan']; // 0011a
+// b1: cấp cho biến users một bộ nhớ trên thanh RAM trên máy tính
+// -------|-------|--------|-------
+//  0011a   0011b   0011c    0011d
+//  users
+// console.log(helloUser(users[0])); // hello hung
+
+// let customers = users; // địa chỉ của biến customers bằng với địa chỉ của biến users
+
+// 0011a = ['hung', 'nhan', 'thuy', 'ngan'];
+
+// customers[0] = 'kha';
+
+// 0011a = ['kha', 'nhan', 'thuy', 'ngan'];
+
+// console.log(users);
+// 0011a: ['kha', 'nhan', 'thuy', 'ngan'];
+
+// users[1] = 'thach';
+// 0011a: ['kha', 'thach', 'thuy', 'ngan'];
+
+// console.log(users);
+// 0011a: ['kha', 'thach', 'thuy', 'ngan'];
+// console.log(customers);
+// 0011a: ['kha', 'thach', 'thuy', 'ngan'];
+
+// console.log(helloUser(users[0])); //hello kha
+// console.log(helloUser(users[1])); //hello thach
+// console.log(helloUser(customers[1])); //hello thach
+
+// function helloUser(user) {
+//   return 'hello ' + user;
+// }
+
+// let customers = users;
+/**
+ * Khi customers thay đổi -> users sẽ bị thay đổi theo
+ * khi users thay đổi -> customers sẽ bị thay đổi theo
+ * --> tại vì users và customers cùng trỏ đến một địa chỉ
+ */
+
+// let users = ['hung', 'nhan', 'thuy', 'ngan'];
+// let customers = [...users];
+/**
+ * b1: let customers = []; //abc02
+ * b2: ...users => 'kha', 'nhan', 'thuy', 'ngan'
+ * b4: let customers = [...users]; <=> let customers = ['kha', 'nhan', 'thuy', 'ngan'];
+ */
+/**
+ * Khi customers thay đổi -> users không bị thay đổi theo
+ * Khi users thay đổi -> customers không bị thay đổi theo
+ * --> users và customers phải trỏ đến 2 địa chỉ khác nhau
+ */
+
+/**
+ * Giải thích về cách lưu trên ô nhớ
+ *
+ * --------|--------|--------|--------
+ *  abc01    abc02    abc03    abc04
+ *  users   customers
+ */
+/**
+ * 1. let đầu tiên
+ *   - Địa chỉ của biến users trên thanh RAM là abc01
+ *   - Giá trị tại <địa chỉ> của biến users là ['kha', 'nhan', 'thuy', 'ngan']
+ *    ==> Giá trị tại abc01 là ['kha', 'nhan', 'thuy', 'ngan']
+ *
+ * 2. let thứ 2
+ *   - Địa chỉ của biến customers trên thanh RAM là abc02
+ *   - Giá trị tại <địa chỉ> của biến customers là ['kha', 'nhan', 'thuy', 'ngan']
+ *    ==> Giá trị tại abc02 là ['kha', 'nhan', 'thuy', 'ngan']
+ */
+
+//  let users = ['hung', 'nhan', 'thuy', 'ngan'];
+//  let customers = [...users];
+
+// customers[0] = 'kha';
+// console.log(users[0]); // hung
+// console.log(customers[0]); // kha
+
+
+// Function filter cua array
+// const listNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const result = listNums.filter((item) => {
+//   if (item % 2 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+
+// console.log(result);
+
+// const users = [
+//   {
+//     id: 1,
+//     name: 'hung'
+//   },
+//   {
+//     id: 2,
+//     name: 'nhan'
+//   }
+// ]
+
+// const search = 'un';
+
+// const result = users.find((item) => {
+//   console.log(item.name);
+//   if () {
+
+//   }
+// });
+
+// console.log(result);
+// undefined, null, 0, false, [] => false
+// 1,2,3,4, => true
--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,92 @@
 import Tasks from '../../data/tasks.js';
 import { convertLevel } from '../../utils/helpers.js';
 
+const sortTypeData = [
+  {
+    sortBy: 'title',
+    orderBy: 'asc',
+  },
+  {
+    sortBy: 'title',
+    orderBy: 'desc',
+  },
+  {
+    sortBy: 'level',
+    orderBy: 'asc',
+  },
+  {
+    sortBy: 'level',
+    orderBy: 'desc',
+  },
+];
+
 init();
 
 function init() {
+  const $searchInput = document.getElementById('search-task-input');
+  const $sortSelect  = document.getElementById('sort-select');
+  const $mainContent = document.getElementById('main-content');
+  const $imageNotFound = document.getElementById('not-found');
+  const $table = document.getElementById('tasks-content');
+  const $sortValue = document.getElementById('sort-value');
 
   renderTasks(Tasks);
+  renderSortList(sortTypeData);
+
+  let searchString = '';
+
+  $searchInput.addEventListener('input', search);
+  $sortSelect.addEventListener('change', sort);
+
+  function search(e) {
+    // Lấy giá trị trong input
+    searchString = e.target.value;
+    // Copy ds task list của mình qua một ds mới
+    // Tại sao phải copy
+    // Để ko bị thay đổi giá trị của task gốc => Tasks
+    let taskListCopy = [...Tasks];
+    const result = taskListCopy.filter((task) => {
+      if ( (task.title.toLowerCase()).includes(searchString.toLowerCase()) ) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    // if (result.length === 0) {
+    //   toggleNotFound(false);
+    // } else {
+    //   toggleNotFound(true);
+    // }
+
+    renderTasks(result);
+  }
+
+  function sort(e) {
+    // e.target => DOM;
+    // e.target.value => giá trị của input
+
+    // Cắt chuỗi thành 2 phần
+    const value = (e.target.value).split('-');
+
+    const sortBy = value[0];
+    const orderBy = value[1];
+
+    $sortValue.innerHTML = `${sortBy.toUpperCase()} - ${orderBy.toUpperCase()}`;
+  }
+
+
+  function toggleNotFound(state) {
+    if (state) {
+      // Có giá trị
+      $mainContent.style.display = 'block';
+      $imageNotFound.style.display = 'none';
+    } else {
+      // Không có giá trị
+      $mainContent.style.display = 'none';
+      $imageNotFound.style.display = 'block';
+    }
+  }
 
   /**
    * Handle render list task into HTML DOM
@@ -13,7 +94,7 @@ function init() {
    * @returns void
    */
   function renderTasks(tasks) {
-    const $table = document.getElementById('tasks-content');
+    toggleNotFound(tasks.length);
 
     let tasksHTMLArray = tasks.map(function(item, index) {
       return `<tr data-id="${item.id}">
@@ -30,7 +111,16 @@ function init() {
     });
 
     const tasksHTMLString = tasksHTMLArray.join('');
-
     $table.innerHTML = tasksHTMLString;
+  }
+
+  function renderSortList(list) {
+    let sortHTMLArray = list.map(function(item) {
+      return `<option value="${item.sortBy}-${item.orderBy}">${(item.sortBy).toUpperCase()} - ${(item.orderBy).toUpperCase()}</option>`;
+    });
+
+    const sortHTMLString  = sortHTMLArray.join('');
+    $sortSelect.innerHTML = sortHTMLString;
+    $sortValue.innerHTML  = `${list[0].sortBy.toUpperCase()} - ${list[0].orderBy.toUpperCase()}`;
   }
 }
